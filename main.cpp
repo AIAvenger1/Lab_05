3a2da94dbf272f1b2cf59958b7b685b7a5768d33
ai_avenger <b.petrov.viktorovich@gmail.com>
2024-05-25 18:12:40 +0300

    First

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include "simple.h"
+#include "flag.h"
+
+using std::pair;
+
+void greetings() //func that just returns information about author and work
+{
+    cout << "\nBohdan Petrov, K13, Lab_05, var_34\n\n";
+    cout << "Press Enter to Continue...";
+    char temp1; while(temp1 != '\n') std::cin.get(temp1);
+
+    cout << "\nWrite sequence in output.txt file with special subsequences(spiral, snake)\n\n";
+    cout << "Press Enter to Continue...";
+    char temp2; while(temp2 != '\n') std::cin.get(temp2);
+}
+
+void farewell()
+{
+  cout << "Program terminates, no problem found. Bye\n";
+}
+
+int main(int argc, char* argv[])
+{
+    greetings(); // greeting message
+    string IN, OUT;
+    if(!get_args(argc, argv, IN, OUT)) //checking argc
+    {
+      help_flag(); // writing help message if something wrong with arguments 
+      return 0;   
+    }
+    ifstream Input(IN); //opening in.txt
+    ofstream Output(OUT, std::ofstream::out | std::ofstream::trunc); //opening out.txt
+    try{
+    Matrix input_maxtrix(Input); //constructing Matrix (see simple.cpp/simple.h) from given txt file
+  
+    pair<vector<int>, vector<int> > sequence{first(input_maxtrix),second(input_maxtrix)};
+    Output << "First part: \n";
+    for(auto&it: sequence.first)
+    {
+      Output << it << " ";
+    }
+    Output << "\nSecond part: \n";
+     for(auto&it: sequence.second)
+    {
+      Output << it << " ";
+    }
+    }
+    catch(const std::invalid_argument& ia){
+      Input.close();
+      Output.close();
+      std::cerr << "Invalid argument: " << ia.what() << '\n';
+      return 0;
+    }
+    Input.close(); //closing txt
+    Output.close(); // closing txt
+    farewell(); // goodbye message
+    
+    return 1;
+ }
+
+
---
+++ simple.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <vector>
+
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::ostream;
+using std::invalid_argument;
+
+class Matrix
+{
+private:
+    int size;
+    int **p;
+
+    void allocSpace();
+public:
+    Matrix();
+    Matrix(int);
+    Matrix(ifstream & is);
+    Matrix(int**, int);
+    Matrix(const Matrix&);
+    Matrix(Matrix&&) = delete;
+    Matrix operator=(const Matrix&)=delete;
+    Matrix operator=(Matrix&&)=delete;
+    ~Matrix();
+
+    int get_size();
+
+    friend std::ostream& operator<<(std::ostream&, const Matrix&); // for check purpose
+
+    inline int operator()(int x, int y) {return p[x][y];}  // operator that just takes (x,y) point in Matrix as you said
+};
+
+vector<int> first(Matrix); // function that returns first required sequence from given matrix
+
+vector<int> second(Matrix); // function that returns second required sequence from given matrix
+
+
